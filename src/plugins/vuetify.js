0f025e94322a40bb34ca6448d51d27dfa3483c84
evoxeed <nikita.gomozov@gmail.com>
2023-05-22 15:02:25 +0400

    add skeleton-loader and optimization vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,21 @@
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
-import * as components from 'vuetify/components'
+import { VIcon, VAvatar, VImg, VCol, VBtn, VRow, VContainer } from 'vuetify/components'
+import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 
 export default createVuetify({
-  components,
+  components: {
+    VIcon,
+    VAvatar,
+    VImg,
+    VCol,
+    VBtn,
+    VRow,
+    VContainer,
+    VSkeletonLoader
+  },
   directives,
   icons: {
     defaultSet: 'mdi',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,10 @@ import { mdiInstagram, mdiTwitch, mdiTwitter, mdiCheck } from '@mdi/js'
 </script>
 
 <template>
-  <main class="bg-grey-darken-4">
+  <main class="bg-grey-darken-4 h-100">
     <v-container fluid>
       <v-row>
-        <v-col cols="12" class="text-center">
+        <v-col cols="12" class="text-center" style="margin-top: 128px">
           <v-avatar size="256">
             <v-img cover src="avatar.jpg" alt="John"></v-img>
           </v-avatar>
@@ -15,7 +15,7 @@ import { mdiInstagram, mdiTwitch, mdiTwitter, mdiCheck } from '@mdi/js'
             <v-icon size="24" class="ml-2">{{ mdiCheck }}</v-icon>
           </div>
         </v-col>
-        <v-col cols="12" class="d-flex justify-center align-center" style="gap: 32px">
+        <v-col cols="12" class="d-flex justify-center align-center mb-16" style="gap: 32px">
           <v-btn theme="dark" icon href="https://instagram.com" target="_blank">
             <v-icon size="32"> {{ mdiInstagram }}</v-icon>
           </v-btn>
@@ -28,24 +28,30 @@ import { mdiInstagram, mdiTwitch, mdiTwitter, mdiCheck } from '@mdi/js'
           </v-btn>
         </v-col>
       </v-row>
+    </v-container>
+    <v-container style="max-width: 1800px">
       <v-row>
         <v-col
-          cols="12"
+          cols="6"
           sm="4"
           md="3"
-          xl="2"
           v-for="i in 10"
           :key="i"
           class="d-flex align-center justify-center"
         >
           <v-img
             cover
-            class="rounded-xl"
+            class="rounded"
             aspect-ratio="1"
             transition="fade"
             width="300"
-            src="avatar.jpg"
-          />
+            height="300"
+            :src="`https://loremflickr.com/320/240`"
+          >
+            <template v-slot:placeholder>
+              <v-skeleton-loader color="grey-darken-3" type="image, image"></v-skeleton-loader>
+            </template>
+          </v-img>
         </v-col>
       </v-row>
     </v-container>
